ea66fe8f16e6539a2b461db7aa87930c7177beb0
SIDDHARTHARARUMUGAM <131138992+SIDDHARTHARARUMUGAM@users.noreply.github.com>
2025-09-20 16:24:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from itertools import combinations
 app = Flask(__name__)
 CORS(app)
 
-IMAGE_FOLDER = "static/images"
+IMAGE_FOLDER = "static/image"
 DATA_FILE = "photos.json"
 INITIAL_ELO = 1200
 PAIRS_FILE = "pairs.json"
@@ -117,4 +117,5 @@ def reset():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
+
     app.run(debug=True, host="0.0.0.0", port=port)

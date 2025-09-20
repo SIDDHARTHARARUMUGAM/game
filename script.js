2cafca4460280bb483414ee9921ea9f702bf2aa2
SIDDHARTHARARUMUGAM <131138992+SIDDHARTHARARUMUGAM@users.noreply.github.com>
2025-09-20 19:45:37 +0530

    Added production url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiUrl = "http://127.0.0.1:5000" // Flask API
+const apiUrl = "https://gamebackendapi.onrender.com/" // Flask API
 
 async function loadRandom() {
   try {
@@ -135,3 +135,4 @@ function showVoteSuccess() {
 document.addEventListener("DOMContentLoaded", () => {
   loadRandom()
 })
+
